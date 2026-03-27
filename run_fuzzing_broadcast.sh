68838b20d4e6712c7898f5225548ead3d7743bed
sckrtge <2962886557@qq.com>
2026-03-27 16:33:24 +0800

    version 0.3.0

--- run_fuzzing_broadcast.sh
+++
@@ -1,6 +0,0 @@
-sudo /home/gxm/projects/.venv/bin/python -m capwap_discovery_fuzzer \
-  --broadcast \
-  --ac-port 5246 \
-  --rounds 1 \
-  --timeout 3 \
-  # --pcap ./pcaps/sample_discovery_request.pcap \
